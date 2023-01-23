bc01745fe320396a9a0d480c3040c637ea34a575
Idan Mariani <idanmariani1234@gmail.com>
2023-01-23 11:30:49 +0200

    add file handler

--- main.c
+++ main.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
 
-
-
-
 int main(int argc, char *argv[])
 {
+    FILE *file;
 
+    if (argc != 2)
+    {
+        printf("Argument number error.\n");
+        printf("Enter ./main customers.txt to run program\n");
+        return 1;
+    }
 
+    file = fopen(argv[1], "r");
+
+    if (file == NULL)
+    {
+        printf("Error opening file.\n");
+        return 1;
+    }
+
+    fclose(file);
 
     return 0;
 }
