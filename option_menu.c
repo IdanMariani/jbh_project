0bf977bb2bc5cffda1942560443b45a1d732033d
Idan Mariani <idanmariani1234@gmail.com>
2023-01-23 15:00:14 +0200

    add select_option_menu func

--- option_menu.c
+++ option_menu.c
@@ -52,3 +52,28 @@ bool found_in_list(Customer *list, int new_list_length, char *portion3, int inde
 
     return false;
 }
+
+void select_option_menu(Customer *list, int *new_list_length, char *buffer, char portion2, char *portion3)
+{
+    char *select_menu[] = {"select first name", "select last name", "select id", "select phone", "select debt", "select date"};
+
+    bool customer_in_list = true;
+
+    for (int i = 0; i < ARR_SIZE(select_menu); i++)
+    {
+        if (strstr(buffer, select_menu[i]))
+        {
+            customer_in_list = found_in_list(list, *new_list_length, portion3, i);
+            if (customer_in_list == true)
+            {
+                print_operator(list, *new_list_length, i, portion2, portion3);
+                break;
+            }
+            else if (customer_in_list == false)
+            {
+                printf("%s was not found in list\n", portion3);
+                break;
+            }
+        }
+    }
+}
--- option_menu.h
+++ option_menu.h
@@ -6,6 +6,7 @@
 #include "validate.h"
 
 bool found_in_list(Customer *list, int new_list_length, char *portion3, int index);
+void select_option_menu(Customer *list, int *new_list_length, char *buffer, char portion2, char *portion3);
 
 
 #endif
