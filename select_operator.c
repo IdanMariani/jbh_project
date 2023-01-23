b6250fa371506aba7c162678c8df92a2f4bb9544
Idan Mariani <idanmariani1234@gmail.com>
2023-01-23 12:39:16 +0200

    Add print_operator func

--- select_operator.c
+++ select_operator.c
@@ -26,3 +26,30 @@ bool fill_portion2_operator(int index, char *portion2)
 
     return false;
 }
+
+void print_operator(Customer *list, int new_list_length, int index, char portion2, char *portion3)
+{
+    switch (index)
+    {
+    case 0:
+        first_name_operator(list, new_list_length, portion2, portion3);
+        break;
+    case 1:
+        last_name_operator(list, new_list_length, portion2, portion3);
+        break;
+    case 2:
+        id_operator(list, new_list_length, portion2, portion3);
+        break;
+    case 3:
+        phone_operator(list, new_list_length, portion2, portion3);
+        break;
+    case 4:
+        debt_operator(list, new_list_length, portion2, portion3);
+        break;
+    case 5:
+        date_operator(list, new_list_length, portion2, portion3);
+        break;
+    default:
+        break;
+    }
+}
--- select_operator.h
+++ select_operator.h
@@ -4,6 +4,7 @@
 #include "general.h"
 
 bool fill_portion2_operator(int index, char *portion2);
+void print_operator(Customer *list, int new_list_length, int index, char portion2, char *portion3);
 
 
 
