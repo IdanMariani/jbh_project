13e53a521be3d62aa1e5b47efa94b5958581a43f
Idan Mariani <idanmariani1234@gmail.com>
2023-01-23 11:45:17 +0200

    Add read_csv, add validate

---
+++ read_csv.h
@@ -0,0 +1,10 @@
+#ifndef READ_CSV_H
+#define READ_CSV_H
+
+#include "general.h"
+#include "validate.h"
+
+
+
+
+#endif
---
+++ validate.h
@@ -0,0 +1,10 @@
+#ifndef VALIDATE_H
+#define VALIDATE_H
+
+#include "general.h"
+#include "read_csv.h"
+
+
+
+
+#endif
