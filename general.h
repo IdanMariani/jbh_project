10766b21ee40437994a0987f803e07feffa3cc87
Idan Mariani <idanmariani1234@gmail.com>
2023-01-23 11:27:50 +0200

    add general.c, general.h

---
+++ general.h
@@ -0,0 +1,22 @@
+#ifndef GENERAL_H
+#define GENERAL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+
+typedef struct
+{
+    char first_name[30];
+    char last_name[30];
+    char id[10];
+    char phone[11];
+    float debt;
+    char date[12];
+} Customer;
+
+
+#endif
