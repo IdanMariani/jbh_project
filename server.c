82e49a4d85e243b3203154fe16579f459d88574b
Idan Mariani <idanmariani1234@gmail.com>
2023-01-28 20:42:16 +0200

    update print_list_server func

--- server.c
+++ server.c
@@ -64,32 +64,7 @@ void *conn_handler(void *args)
 
     if (strcmp(new_buffer, "print") == 0)
     {
-        sprintf(buffer_send, "%-15s%-15s%-15s%-15s%-15s%-15s\n", "First Name", "Last Name", "ID", "Phone", "Debt", "Date");
-        n = send(new_sock, buffer_send, strlen(buffer_send), 0);
-        if (n < 0)
-        {
-            perror("Server error sending data");
-            goto exit;
-        }
-        sprintf(buffer_send, "%s\n", "*************************************************************************************");
-        n = send(new_sock, buffer_send, strlen(buffer_send), 0);
-        if (n < 0)
-        {
-            perror("Server error sending data");
-            goto exit;
-        }
-        for (int i = 0; i < new_list_length; i++)
-        {
-            sprintf(buffer_send, "%-15s%-15s%-15s%-15s%-15.2f%-15s\n", list[i].first_name, list[i].last_name,
-                    list[i].id, list[i].phone, list[i].debt, list[i].date);
-            n = send(new_sock, buffer_send, strlen(buffer_send), 0);
-            if (n < 0)
-            {
-                perror("Server error sending data");
-                goto exit;
-            }
-        }
-
+        print_list_server(list,new_list_length);
         goto exit;
     }
 
